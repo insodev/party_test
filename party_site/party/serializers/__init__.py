22b73c5037e603b46e82f89c6275134470133e16
Dmitry V Podshchekoldin <dv@insarm.ru>
2019-08-28 20:16:47 +0300

    Initial commit

---
+++ party_site/party/serializers/__init__.py
@@ -0,0 +1,8 @@
+from .party import PartyShortSerializer, PartyDetailSerializer
+from .user import UserSerializer
+
+__all__ = [
+    'PartyShortSerializer',
+    'UserSerializer',
+    'PartyDetailSerializer',
+]
